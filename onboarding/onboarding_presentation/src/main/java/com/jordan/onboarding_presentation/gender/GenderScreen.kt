2275802599a27863c0efbc94743e32734030dd95
asxz0 <asxz0121@gmail.com>
2022-06-26 18:48:15 +0800

    Preferences setup and onboard components

---
+++ onboarding/onboarding_presentation/src/main/java/com/jordan/onboarding_presentation/gender/GenderScreen.kt
@@ -0,0 +1,7 @@
+package com.jordan.onboarding_presentation.gender
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GenderScreen() {
+}
--- onboarding/onboarding_presentation/src/main/java/com/jordan/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/jordan/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -4,13 +4,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.jordan.core.R
 import com.jordan.core.navigation.Route
 import com.jordan.core.util.UiEvent
