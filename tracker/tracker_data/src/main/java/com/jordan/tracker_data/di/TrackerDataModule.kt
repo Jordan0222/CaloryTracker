5000d9e44ff8895072c90b46d5b614597dc41be7
asxz0 <asxz0121@gmail.com>
2022-06-28 20:04:02 +0800

    open food api and local database

---
+++ tracker/tracker_data/src/main/java/com/jordan/tracker_data/di/TrackerDataModule.kt
@@ -0,0 +1,54 @@
+package com.jordan.tracker_data.di
+
+import android.app.Application
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.jordan.tracker_data.local.TrackerDatabase
+import com.jordan.tracker_data.remote.OpenFoodApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TrackerDataModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenFoodApi(okHttpClient: OkHttpClient): OpenFoodApi {
+        return Retrofit.Builder()
+            .baseUrl(OpenFoodApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTrackerDatabase(app: Application): TrackerDatabase {
+        return Room.databaseBuilder(
+            app,
+            TrackerDatabase::class.java,
+            "tracker_db"
+        ).build()
+    }
+}
