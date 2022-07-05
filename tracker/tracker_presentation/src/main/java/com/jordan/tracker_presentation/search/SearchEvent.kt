d2de1fb3f0a269fcae92045332acf4ca568ffe67
asxz0 <asxz0121@gmail.com>
2022-07-05 22:26:57 +0800

    TrackFood Item

---
+++ tracker/tracker_presentation/src/main/java/com/jordan/tracker_presentation/search/SearchEvent.kt
@@ -0,0 +1,20 @@
+package com.jordan.tracker_presentation.search
+
+import com.jordan.tracker_domain.model.MealType
+import com.jordan.tracker_domain.model.TrackableFood
+import java.time.LocalDate
+
+sealed class SearchEvent {
+    data class  OnQueryChange(val query: String): SearchEvent()
+    object OnSearch: SearchEvent()
+    data class OnToggleTrackableFood(val food: TrackableFood): SearchEvent()
+    data class OnAmountForFoodChange(
+        val food: TrackableFood,
+        val amount: String
+    ): SearchEvent()
+    data class OnTrackFoodClick(
+        val food: TrackableFood,
+        val mealType: MealType,
+        val localDate: LocalDate
+    ): SearchEvent()
+}
--- tracker/tracker_presentation/src/main/java/com/jordan/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/jordan/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,20 +1,17 @@
 package com.jordan.tracker_presentation.tracker_overview
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.jordan.core.util.UiEvent
 import com.jordan.core_ui.LocalSpacing
-import com.jordan.tracker_presentation.tracker_overview.components.DaySelector
-import com.jordan.tracker_presentation.tracker_overview.components.ExpandableMeal
-import com.jordan.tracker_presentation.tracker_overview.components.NutrientsHeader
+import com.jordan.tracker_presentation.tracker_overview.components.*
+import com.jordan.core.R
 
 @Composable
 fun TrackerOverviewScreen(
@@ -52,7 +49,33 @@ fun TrackerOverviewScreen(
                 onToggleClick = {
                     viewModel.onEvent(TrackerOverViewEvent.OnToggleMealClick(meal))
                 },
-                content = {  },
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(spacing.spaceSmall)
+                    ) {
+                        state.trackedFoods.forEach { food ->
+                            TrackedFoodItem(
+                                trackedFood = food,
+                                onDeleteClick = {
+                                    viewModel.onEvent(TrackerOverViewEvent.OnDeleteTrackedFoodClick(food))
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                        }
+                        AddButton(
+                            text = stringResource(
+                                id = R.string.add_meal,
+                                meal.name.asString(context)
+                            ),
+                            onClick = {
+                                viewModel.onEvent(TrackerOverViewEvent.OnAddFoodClick(meal))
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                },
                 modifier = Modifier.fillMaxWidth()
             )
         }
